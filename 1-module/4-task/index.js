5a139a8d7d5ea1bf076d13aafb177508b4656220
BorisNovi <borisnovikov4815@gmail.com>
2022-09-08 17:33:34 +0300

    returned 1-st version in a 4-th task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,11 +1,8 @@
 function checkSpam(str) {
   let strDown = str.toLowerCase();
-  if (strDown.includes(`1xbet`) || strDown.includes(`xxx`)) {
-      return true;
-  } else return false;
+  return strDown.includes(`1xbet`) || strDown.includes(`xxx`) 
 }
 
 console.log(checkSpam('1XbeT now'))
 console.log(checkSpam('free XXX')) 
 console.log(checkSpam('innocent rabbit'))
-
