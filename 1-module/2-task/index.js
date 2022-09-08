625cc2f604adf7e95cf9a9740e88ce9fd8a74979
BorisNovi <borisnovikov4815@gmail.com>
2022-09-08 11:22:42 +0300

    tasks 1.1 - 1.3 complete and cecked, tasks 1.4 - 1.5 check failed

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,9 +9,31 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid(name) {
-  // ваш код...
+ function isValid(name){
+
+  if (name == null) {
+      console.log(`namespace is null`);
+      return false; 
+      
+  } else if ( name == ``) {
+      console.log(`namespace is empty`);
+      return false;
+      
+  } else if ( name.length < 4) {
+      console.log(`name sthorter than 4`);
+      return false;
+      
+  } else if (name.includes(' ')) {
+      console.log(`name includes spaces`);
+      return false;
+       
+  }      
+      else 
+      (console.log(`passed: ${name}`));
+      return true ;
+
 }
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
