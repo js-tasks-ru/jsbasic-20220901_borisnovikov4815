09a802369c2bdfbd2b80a972711e35b10ecd5eb9
BorisNovi <borisnovikov4815@gmail.com>
2022-09-08 15:11:51 +0300

    all tasks are done

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  let strDown = str.toLowerCase();
+  if (strDown.includes(`1xbet`) || strDown.includes(`xxx`)) {
+      return true;
+  } else return false;
 }
+
+console.log(checkSpam('1XbeT now'))
+console.log(checkSpam('free XXX')) 
+console.log(checkSpam('innocent rabbit'))
+
