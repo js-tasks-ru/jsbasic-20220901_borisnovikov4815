6af1d4810b43b6748ed776672f6ecbaa430b7552
BorisNovi <borisnovikov4815@gmail.com>
2022-09-12 20:21:12 +0300

    all tasks are done, tests ok

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,10 +4,9 @@ function sumSalary(salaries) {
     for (let key in salaries) {
      
       if (isNaN(salaries[key]) === true || isFinite(salaries[key]) === false) continue;
-      //if (typeof salaries[key] === `number`) {
        sum += salaries[key];
         console.log(isNaN(salaries[key]));
-       //}
+       
     
    }
     return sum
