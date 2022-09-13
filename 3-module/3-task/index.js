e74eb3c5b6d20755bf04ef84c0abf76cd7708bc7
BorisNovi <borisnovikov4815@gmail.com>
2022-09-13 19:54:24 +0300

    task 3-3 done

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
-function camelize(str) {
-  // ваш код...
+camelize('background-color') == 'backgroundColor';
+camelize('list-style-image') == 'listStyleImage';
+camelize('-webkit-transition') == 'WebkitTransition';
+
+function camelize(str){
+  return str.split('-') 
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
 }
+
+
+console.log(camelize('background-color'));
+console.log(camelize('list-style-image'));
+console.log(camelize('-webkit-transition'));
