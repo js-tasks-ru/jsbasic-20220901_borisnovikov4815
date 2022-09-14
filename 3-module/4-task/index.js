e90524656bc04e521fe291663e52d4e9217f5f7e
BorisNovi <borisnovikov4815@gmail.com>
2022-09-14 14:53:17 +0300

    task 3-4 done

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,15 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(users, age)
+{
+  let obj = '';
+  
+  for (let i of users)
+  { 
+   //  console.log(i[`age`]); // перебираем объекты и выводим их возраст в консоль
+    if (i[`age`] <= age)
+    {   // если возраст в свойстве 'age' <= возрасту в аргументе функции
+      obj += (`${i[`name`]}, ${i[`balance`]}\n`); 
+ // console.log(obj)     // то выводим 'name' и 'balance' этих объектов в консоль  
+    }
+  }
+  return obj.trim()
 }
