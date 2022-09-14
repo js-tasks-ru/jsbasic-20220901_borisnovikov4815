08bf2e099ac2b3a465fcf224143d385b67ef68d1
BorisNovi <borisnovikov4815@gmail.com>
2022-09-14 20:47:39 +0300

    3-5 done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,22 @@
 function getMinMax(str) {
-  // ваш код...
+  let arrayOfShit = str
+  .split(' ')
+  .filter(item => +item)
+  .map(item => Number(item))
+  .sort(compareNumeric);
+  
+  console.log(arrayOfShit);
+  
+  function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
+}
+  
+let result = 
+    {min: Number(arrayOfShit.slice(0, 1)), 
+     max: Number(arrayOfShit.slice(-1)),
+             }
+  
+  return result
 }
