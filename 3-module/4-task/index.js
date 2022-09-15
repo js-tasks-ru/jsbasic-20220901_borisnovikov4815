618083a395591c2195d6696be0a145578ec3df28
BorisNovi <borisnovikov4815@gmail.com>
2022-09-15 19:13:58 +0300

    код исправлен на вариант с использованием методов масиива

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,15 +1,8 @@
 function showSalary(users, age)
 {
   let obj = '';
-  
-  for (let i of users)
-  { 
-   //  console.log(i[`age`]); // перебираем объекты и выводим их возраст в консоль
-    if (i[`age`] <= age)
-    {   // если возраст в свойстве 'age' <= возрасту в аргументе функции
-      obj += (`${i[`name`]}, ${i[`balance`]}\n`); 
- // console.log(obj)     // то выводим 'name' и 'balance' этих объектов в консоль  
-    }
-  }
-  return obj.trim()
-}
+  obj = users
+    .filter(item => item.age <= age)
+    .map(item => item.name + `, ` + item.balance).join(`\n`);
+  return obj
+ }
